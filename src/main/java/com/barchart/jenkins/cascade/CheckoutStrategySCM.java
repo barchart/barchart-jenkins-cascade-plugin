37a7375f5150da05e95ccf07f30edb0b44e5dcb7
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2013-03-06 22:54:12 -0600

    scm stragegy

---
+++ src/main/java/com/barchart/jenkins/cascade/CheckoutStrategySCM.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (C) 2013 Barchart, Inc. <http://www.barchart.com/>
+ *
+ * All rights reserved. Licensed under the OSI BSD License.
+ *
+ * http://www.opensource.org/licenses/bsd-license.php
+ */
+package com.barchart.jenkins.cascade;
+
+import hudson.Extension;
+import hudson.model.BuildListener;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractBuild.AbstractBuildExecution;
+import hudson.model.AbstractProject;
+
+import java.io.IOException;
+
+import jenkins.scm.SCMCheckoutStrategyDescriptor;
+import jenkins.scm.SCMCheckoutStrategy;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * Checkout strategy that pays attention to custom actions.
+ * 
+ * @author Andrei Pozolotin
+ */
+@Extension
+public class CheckoutStrategySCM extends SCMCheckoutStrategy {
+
+	@Extension
+	public static class TheDescriptor extends SCMCheckoutStrategyDescriptor {
+		@Override
+		public String getDisplayName() {
+			return "Custom";
+		}
+
+		@Override
+		public boolean isApplicable(final AbstractProject project) {
+			return true;
+		}
+	}
+
+	@DataBoundConstructor
+	public CheckoutStrategySCM() {
+	}
+
+	@Override
+	public void checkout(final AbstractBuildExecution execution)
+			throws IOException, InterruptedException {
+
+		final AbstractBuild build = (AbstractBuild) execution.getBuild();
+		final BuildListener listener = execution.getListener();
+
+		final BuildContext context = new BuildContext(build, listener);
+
+		final CheckoutSkipAction skipAction = build
+				.getAction(CheckoutSkipAction.class);
+
+		if (skipAction == null) {
+			context.log("Normal checkout.");
+			super.checkout(execution);
+		} else {
+			context.log("Ingoring checkout.");
+		}
+
+	}
+
+}
---
+++ src/test/java/design/PluginLogger.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C) 2013 Barchart, Inc. <http://www.barchart.com/>
+ *
+ * All rights reserved. Licensed under the OSI BSD License.
+ *
+ * http://www.opensource.org/licenses/bsd-license.php
+ */
+package design;
+
+import hudson.model.TaskListener;
+
+import com.barchart.jenkins.cascade.PluginConstants;
+
+/**
+ * Convenience build logger.
+ * 
+ * @author Andrei Pozolotin
+ */
+public class PluginLogger {
+
+	private final TaskListener listener;
+
+	public PluginLogger(final TaskListener listener) {
+		this.listener = listener;
+	}
+
+	/** Log text with plug-in prefix. */
+	public void text(final String text) {
+		listener.getLogger()
+				.println(PluginConstants.LOGGER_PREFIX + " " + text);
+	}
+
+}
